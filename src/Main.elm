6f0e259bc710a357f991bff36992b5997a4122a2
mkusaka <hinoshita1992@gmail.com>
2019-12-29 19:23:32 +0900

    list

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,64 @@
-module Main exposing (add, output)
+module Main exposing (Model, Msg(..), init, main, update, view)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
-output =
-    "1 + 1 = " ++ String.fromInt (add 1 1)
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
-add a b =
-    a + b
+type alias Model =
+    { input : String
+    , memos : List String
+    }
+
+
+init : Model
+init =
+    { input = ""
+    , memos = []
+    }
+
+
+type Msg
+    = Input String
+    | Submit
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input input ->
+            { model | input = input }
+
+        Submit ->
+            { model
+                | input = ""
+                , memos = model.input :: model.memos
+            }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.form [ onSubmit Submit ]
+            [ input [ value model.input, onInput Input ] []
+            , button
+                [ disabled (String.length model.input < 1) ]
+                [ text "Submit" ]
+            ]
+        , ul [] (List.map viewMemo model.memos)
+        ]
+
+
+viewMemo : String -> Html Msg
+viewMemo memo =
+    li [] [ text memo ]
